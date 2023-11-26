75dd12dcf0f337eda2edb40ab1dbd42919a24a24
Enrique <enrique.flores@utec.edu.pe>
2023-11-25 22:44:34 -0500

    Added method switch to Embedder

--- Embeddings/Embed.py
+++ Embeddings/Embed.py
@@ -14,21 +14,35 @@ from typing import Annotated, Literal, Optional
 
 import logging
 
+from enum import Enum
+
+
+class Method(Enum):
+    mfcc = 1
+    contrast = 2
+
 
 class Embedder():
 
     def __init__(self,
                  sample_rate: int = 22050,
-                 max_duration_seconds: Optional[float] = None):
+                 max_duration_seconds: Optional[float] = None,
+                 method: Method = Method.contrast):
         self.sample_rate = sample_rate
         self.max_duration_seconds = max_duration_seconds
+        self.method = method
 
-    def get_mfcc_features_flatenized(self, song_path: Path) -> np.ndarray:
+    def get_features(self, song_path: Path) -> np.ndarray:
         """Extract the mfcc features for each song in a path"""
 
         data, sr = librosa.load(song_path,
                                 sr=self.sample_rate,
                                 duration=self.max_duration_seconds)
-        mfcc = librosa.feature.mfcc(y=data, sr=sr)
-        chroma_cqt = librosa.feature.chroma_vqt(y=data, sr=sr)
-        return mfcc.flatten()
+        match self.method:
+            case Method.mfcc:
+                mfcc = librosa.feature.mfcc(y=data, sr=sr)
+                return mfcc.flatten()
+            case Method.contrast:
+                spectral_features = librosa.feature.spectral_contrast(y=data,
+                                                                      sr=sr)
+                return spectral_features.flatten()
